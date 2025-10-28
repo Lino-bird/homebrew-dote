ad827fde90a7ee202815774aad06e684e816f6a9
Lino-bird <lino.nikoniko7@gmail.com>
2025-10-28 11:22:08 +0900

    version “1.0.0”

--- dote.rb
+++ dote.rb
@@ -3,6 +3,7 @@ class Dote < Formula
   homepage "https://github.com/Lino-bird/homebrew-dote"
   url "https://raw.githubusercontent.com/Lino-bird/homebrew-dote/main/dote.c"
   sha256 "55678fb51d6beae829c7843148d20afd29c1226a56912f26f0362ee9b4042e3e"
+  version “1.0.0”
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
