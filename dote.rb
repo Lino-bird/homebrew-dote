270120e79ed00d81cb8a75bb189c8cd46a5c5d5f
Lino-bird <lino.nikoniko7@gmail.com>
2025-10-28 11:20:35 +0900

    Update SHA256 checksum

--- dote.rb
+++ dote.rb
@@ -2,7 +2,7 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/Lino-bird/homebrew-dote"
   url "https://raw.githubusercontent.com/Lino-bird/homebrew-dote/main/dote.c"
-  sha256 "PUT_SHA256_OF_DOTE.C_HERE"
+  sha256 "55678fb51d6beae829c7843148d20afd29c1226a56912f26f0362ee9b4042e3e"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
